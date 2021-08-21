25bce418b3366ac43a5164672ad3cdc297b30b27
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 12:30:48 -0400

    added sandbox launch ascii art

--- src/main.c
+++ src/main.c
@@ -7,6 +7,14 @@ void NoArgPrompt()
 {
     printf("Kane - Version 0.0.0.1");
 }
+void OpenSandboxPrompt()
+{
+    printf(" __  __     ______     __   __     ______    \n");
+    printf('/\\ \\/ /    /\\  __ \\   /\\ "-.\\ \\   /\\  ___\\   \n');
+    printf('\\ \\  _"-.  \\ \\  __ \\  \\ \\ \\-.  \\  \\ \\  __\\   \n');
+    printf(' \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\"\\_\\  \\ \\_____\\ \n');
+    printf('  \\/_/\\/_/   \\/_/\\/_/   \\/_/ \\/_/   \\/_____/ \n');
+}
 int main(int argc, char* argv[])
 {
     int RUN_ARG_LEN = 2;
@@ -27,13 +35,16 @@ int main(int argc, char* argv[])
     {
         if (argc > RUN_ARG_LEN)
         {
-            printf("Run Error: Too many arguments supplied");
+            printf("Run Error: Too many arguments supplied\n");
         }
         if (argc < RUN_ARG_LEN)
         {
-            printf("Run Error: Not enough arguments supplied");
+            printf("Run Error: Not enough arguments supplied\n");
         }
-        
-        
+    }
+
+    if(strcmp(argv[1], "sandbox")==0)
+    {
+
     }
 }
