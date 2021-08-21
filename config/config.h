07855117fcfdae517803846ad56464d4000f7d60
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-20 22:06:43 -0400

    made default imports

--- config/config.h
+++ config/config.h
@@ -1,6 +1,7 @@
 #ifndef KANE_CONFIG
 #define KANE_CONFIG
 
+
 extern char OUT_FILE_EXT[] = "kno";
 extern char LANG_FILE_EXT[] = "kn";
 
--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-
+#include "config/config.h"
 int main()
 {
     
