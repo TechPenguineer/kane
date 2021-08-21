4cd796eb47dcf0fcc8d8caf4abe48b292b232fe2
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 11:02:31 -0400

    added argument supplation errors

--- src/main.c
+++ src/main.c
@@ -3,13 +3,37 @@
 #include <string.h>
 #include "include/configs.h"
 
-int main(int argc, char **argv[])
+void NoArgPrompt()
 {
+    printf("Kane - Version 0.0.0.1");
+}
+int main(int argc, char* argv[])
+{
+    int RUN_ARG_LEN = 2;
+
     char OUT_FILE_EXT[] = "kno";
     char LANG_FILE_EXT[] = "kn";
 
+    if(argc==0)
+    {
+        NoArgPrompt();
+    }
+    if(argc==1 && strcmp(argv[1],"--version")==0)
+    {
+        NoArgPrompt();
+    }
+
     if(strcmp(argv[1],"run")==0)
     {
-        printf("%s",argv[1]);
+        if (argc > RUN_ARG_LEN)
+        {
+            printf("Run Error: Too many arguments supplied");
+        }
+        if (argc < RUN_ARG_LEN)
+        {
+            printf("Run Error: Not enough arguments supplied");
+        }
+        
+        
     }
 }
